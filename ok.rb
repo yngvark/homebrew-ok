829a9ba13709c4708b92cadc080409b01f067514
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-04-08 13:13:02 +0200

    wip

--- ok.rb
+++ ok.rb
@@ -4,14 +4,14 @@
 class Ok < Formula
   desc "okctl2-poc testing"
   homepage "https://github.com/yngvark/okctl2-poc"
-  version "0.0.3"
+  version "0.0.4"
   license "AGPL-3.0-only"
 
   depends_on "hashicorp/tap/terraform"
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raw.githubusercontent.com/yngvark/okctl2-poc/main/ok"
+      url "https://github.com/yngvark/okctl2-poc/releases/download/0.0.4/ok"
       sha256 "502502adce76ae35b3222e1c518588780f76d8d52aba60d567add6c5f95a420d"
 
       def install
